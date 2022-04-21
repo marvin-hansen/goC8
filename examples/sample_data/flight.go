885958e8b6912e10e525a893dd9f688f36750826
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-21 19:04:54 +0800

    Updated sample data

--- examples/sample_data/flight.go
+++ examples/sample_data/flight.go
@@ -2,24 +2,27 @@ package sample_data
 
 func GetCityData() []byte {
 	// Document collection named cities.
-	return []byte(` 
-		{"_key": "sanfrancisco", "location": [-122.416667, 37.783333]},
-		{"_key": "newyork", "location": [-74.0059, 40.7127]},
-		{"_key": "detroit", "location": [-83.045833, 42.331389]},
-		{"_key": "paris", "location": [2.3508, 48.8567]},
-		{"_key": "dublin", "location": [-6.260278, 53.349722]},
-		{"_key": "singapore", "location": [103.8, 1.3]},
-		{"_key": "houston", "location": [-95.383056, 29.762778]},
-		{"_key": "seattle", "location": [-122.333056, 47.609722]}
+	return []byte(`[
+    {"_key": "sanfrancisco", "location": [-122.416667, 37.783333]},
+    {"_key": "newyork", "location": [-74.0059, 40.7127]},
+    {"_key": "detroit", "location": [-83.045833, 42.331389]},
+    {"_key": "paris", "location": [2.3508, 48.8567]},
+    {"_key": "dublin", "location": [-6.260278, 53.349722]},
+    {"_key": "singapore", "location": [103.8, 1.3]},
+    {"_key": "houston", "location": [-95.383056, 29.762778]},
+    {"_key": "seattle", "location": [-122.333056, 47.609722]}
+]
 `)
 }
 
 func GetFlightData() []byte {
 	return []byte(` 
+		[
 		{"_from": "cities/sanfrancisco", "_to": "cities/singapore", "distance": 13600},
 		{"_from": "cities/sanfrancisco", "_to": "cities/newyork", "distance": 4000},
 		{"_from": "cities/sanfrancisco", "_to": "cities/detroit", "distance": 3300},
 		{"_from": "cities/sanfrancisco", "_to": "cities/houston", "distance": 2600},
+		{"_from": "cities/sanfrancisco", "_to": "cities/seattle", "distance": 807},
 		{"_from": "cities/detroit", "_to": "cities/sanfrancisco", "distance": 3300},
 		{"_from": "cities/detroit", "_to": "cities/newyork", "distance": 800},
 		{"_from": "cities/newyork", "_to": "cities/sanfrancisco", "distance": 4000},
@@ -27,6 +30,7 @@ func GetFlightData() []byte {
 		{"_from": "cities/newyork", "_to": "cities/dublin", "distance": 5100},
 		{"_from": "cities/newyork", "_to": "cities/paris", "distance": 5800},
 		{"_from": "cities/newyork", "_to": "cities/houston", "distance": 2300},
+		{"_from": "cities/newyork", "_to": "cities/seattle", "distance": 2831},
 		{"_from": "cities/dublin", "_to": "cities/newyork", "distance": 5100},
 		{"_from": "cities/dublin", "_to": "cities/paris", "distance": 800},
 		{"_from": "cities/paris", "_to": "cities/newyork", "distance": 5800},
@@ -36,6 +40,28 @@ func GetFlightData() []byte {
 		{"_from": "cities/singapore", "_to": "cities/paris", "distance": 10700},
 		{"_from": "cities/houston", "_to": "cities/sanfrancisco", "distance": 2600},
 		{"_from": "cities/houston", "_to": "cities/newyork", "distance": 2300}
+]
+`)
+}
+
+func GetAirlineGraph() []byte {
+	return []byte(`
+{
+  "edgeDefinitions": [
+    {
+      "collection": "flights",
+      "from": [
+        "cities"
+      ],
+      "to": [
+        "cities"
+      ]
+    }
+  ],
+  "name": "airline",
+  "options": {}
+}
+
 `)
 }
 
---
+++ examples/university/main.go
@@ -0,0 +1,29 @@
+package university
+
+import "github.com/marvin-hansen/goC8"
+
+func main() {
+
+}
+
+func setup(c *goC8.Client) {
+	setupCourses(c)
+	setupTeachers(c)
+	setupGraph(c)
+}
+
+func setupCourses(c *goC8.Client) {
+
+}
+
+func setupTeachers(c *goC8.Client) {
+
+}
+
+func setupGraph(c *goC8.Client) {
+
+}
+
+func teardown(c *goC8.Client) {
+
+}
