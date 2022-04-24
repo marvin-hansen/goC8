20961a8ee2f1c93f256f422973cb790bae16c458
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-24 15:42:05 +0800

    Updated default geo fabric in default config

--- client_config.go
+++ client_config.go
@@ -1,7 +1,7 @@
 package goC8
 
 const (
-	defaultEndpoint = "https://api-chub-697f556b-ap-south.paas.macrometa.io/_fabric/"
+	defaultEndpoint = "https://api-bigmouth-2265268a-ap-south.paas.macrometa.io/_fabric/" //"https://api-chub-697f556b-ap-south.paas.macrometa.io/_fabric/"
 	defaultFabric   = "SouthEastAsia"
 	// 	collName := "TestCollection"
 	apiKEY = KEY
