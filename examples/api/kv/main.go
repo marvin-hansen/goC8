320aa680de39b23e1da9bf7bfb79bd4b170d6448
Marvin Hansen <marvin.hansen@gmail.com>
2022-05-03 11:44:11 +0800

    Updated KV example

--- examples/api/kv/main.go
+++ examples/api/kv/main.go
@@ -26,12 +26,12 @@ func main() {
 	c := goC8.NewClient(config)
 
 	println("Create KV collection: " + collectionName)
-	res, err := c.CreateNewKVCollection(fabric, collectionName, expiration, nil)
+	res, err := c.KV.CreateNewKVCollection(fabric, collectionName, expiration, nil)
 	goC8.CheckErrorLog(err, "Error creatin KV collection"+collectionName)
 	goC8.PrintRes(res, verbose)
 
 	println("Get all KV collection: ")
-	res, err = c.GetAllKVCollections(fabric)
+	res, err = c.KV.GetAllKVCollections(fabric)
 	goC8.CheckErrorLog(err, "Error getting KV collection"+collectionName)
 	goC8.PrintRes(res, verbose)
 
@@ -43,12 +43,12 @@ func main() {
 	kvPair5 := kv_req2.NewKVPair("key5", "value5", -1)
 
 	kvCollection := kv_req2.NewKVPairCollection(*kvPair1, *kvPair2, *kvPair3, *kvPair4, *kvPair5)
-	resSet, errSet := c.SetKeyValuePairs(fabric, collectionName, *kvCollection)
+	resSet, errSet := c.KV.SetKeyValuePairs(fabric, collectionName, *kvCollection)
 	goC8.CheckErrorLog(errSet, "Error setting values into KV collection"+collectionName)
 	goC8.PrintRes(resSet, verbose)
 
 	println("Count all items in KV collection: ")
-	resCount, errCount := c.CountKVCollection(fabric, collectionName)
+	resCount, errCount := c.KV.CountKVCollection(fabric, collectionName)
 	goC8.CheckErrorLog(errCount, "Error counting values into KV collection"+collectionName)
 	goC8.PrintRes(resCount, verbose)
 
@@ -57,23 +57,23 @@ func main() {
 	limit := 20
 	order := kv_req2.Ascending
 
-	resGetKeys, errGetKeys := c.GetAllKeys(fabric, collectionName, offset, limit, order)
+	resGetKeys, errGetKeys := c.KV.GetAllKeys(fabric, collectionName, offset, limit, order)
 	goC8.CheckErrorLog(errGetKeys, "Error getting all keys from KV collection"+collectionName)
 	goC8.PrintRes(resGetKeys, verbose)
 
 	println("Get all values from KV collection: ")
-	resGetValues, errGetValues := c.GetAllValues(fabric, collectionName, offset, limit, nil)
+	resGetValues, errGetValues := c.KV.GetAllValues(fabric, collectionName, offset, limit, nil)
 	goC8.CheckErrorLog(errGetValues, "Error getting all values from KV collection"+collectionName)
 	goC8.PrintRes(resGetValues, verbose)
 
 	println("Get single value from KV collection: ")
 	key := "value1"
-	resOneVal, errOneVal := c.GetValue(fabric, collectionName, key)
+	resOneVal, errOneVal := c.KV.GetValue(fabric, collectionName, key)
 	goC8.CheckErrorLog(errOneVal, "Error getting one value from KV collection"+collectionName)
 	goC8.PrintRes(resOneVal, verbose)
 
 	println("Delete single value from KV collection: ")
-	resOneDel, errOneDel := c.DeleteValue(fabric, collectionName, key)
+	resOneDel, errOneDel := c.KV.DeleteValue(fabric, collectionName, key)
 	goC8.CheckErrorLog(errOneDel, "Error deleting one value from KV collection"+collectionName)
 	goC8.PrintRes(resOneDel, verbose)
 
@@ -82,20 +82,20 @@ func main() {
 	kvPairUpdate2 := kv_req2.NewKVPair("key2", "value23", -1)
 
 	kvCollectionUpdate := kv_req2.NewKVPairCollection(*kvPairUpdate1, *kvPairUpdate2)
-	resUpd, errUpd := c.SetKeyValuePairs(fabric, collectionName, *kvCollectionUpdate)
+	resUpd, errUpd := c.KV.SetKeyValuePairs(fabric, collectionName, *kvCollectionUpdate)
 	goC8.CheckErrorLog(errUpd, "Error updating values into KV collection"+collectionName)
 	goC8.PrintRes(resUpd, verbose)
 
 	println("Get updated values from KV collection: ")
-	resGetValues, errGetValues = c.GetAllValues(fabric, collectionName, offset, limit, nil)
+	resGetValues, errGetValues = c.KV.GetAllValues(fabric, collectionName, offset, limit, nil)
 	goC8.CheckErrorLog(errGetValues, "Error getting all values from KV collection"+collectionName)
 	goC8.PrintRes(resGetValues, verbose)
 
 	if delete {
-		_, errTruncate := c.TruncateKVCollection(fabric, collectionName)
+		_, errTruncate := c.KV.TruncateKVCollection(fabric, collectionName)
 		goC8.CheckErrorLog(errTruncate, "Error truncating all values in KV collection"+collectionName)
 
-		_, err = c.DeleteKVCollection(fabric, collectionName)
+		_, err = c.KV.DeleteKVCollection(fabric, collectionName)
 		goC8.CheckErrorLog(errTruncate, "Error deleting KV collection"+collectionName)
 	}
 
