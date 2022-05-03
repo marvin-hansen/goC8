e512473548e342b6c1ae87712df90b518ce1b3a1
Marvin Hansen <marvin.hansen@gmail.com>
2022-05-03 18:11:20 +0800

    Added API docs to CollectionManager

--- client_manager_collection.go
+++ client_manager_collection.go
@@ -13,6 +13,11 @@ func NewCollectionManager(client *Client) *CollectionManager {
 	return &CollectionManager{client: client}
 }
 
+// GetAllCollections
+// Returns an object with an attribute collections containing an array of all collection descriptions.
+// The same information is also available in the names as an object with the collection names as keys.
+// By providing the optional query parameter excludeSystem with a value of true, all system collections will be excluded from the response.
+// https://macrometa.com/docs/api#/operations/handleCommandGet
 func (c CollectionManager) GetAllCollections(fabric string) (response *collection_req.ResponseForGetAllCollections, err error) {
 	req := collection_req.NewRequestForGetAllCollections(fabric)
 	response = collection_req.NewResponseForGetAllCollections()
@@ -22,6 +27,27 @@ func (c CollectionManager) GetAllCollections(fabric string) (response *collectio
 	return response, nil
 }
 
+// CreateNewCollection
+// Creates a new collection with a given name. The request must contain an object with the following attributes.
+// name: The name of the collection.
+//keyOptions:
+//allowUserKeys: If set to true, you can supply a key value in the _key attribute of a document. If set to false, the key generator creates keys, and an error occurs if you add a _key value.
+//type: Choose the type of key generator:
+//traditional: generates numerical keys in ascending order.
+//autoincrement: generates numerical keys in ascending order with configurable initial offset and spacing.
+//padded: generates keys of a fixed length (16 bytes) in ascending lexicographical sort order.
+//uuid64: generates universally unique keys.
+//increment: increment value for autoincrement key generator. Not used for other key generator types.
+//offset: Initial offset value for autoincrement key generator. Not used for other key generator types.
+//isSystem: If true, create a system collection. The collection-name must start with an underscore. Not Applicable for end-users. (The default is false)
+//isLocal: If true, create a local collection. For a local collection data is not replicated across regions. (The default is false)
+//type: The type of the collection to be created.
+//The following values for type are valid (The default is 2):
+//2: document collection
+//3: edge collection.
+//stream: If true, create a local stream for collection. (The default is false)
+//shardKeys: The specified shard key determines in which shard a given document is to be stored. Choosing the right shard key can have significant impact on your performance can reduce network traffic and increase performance.
+// https://macrometa.com/docs/api#/operations/handleCommandPost:CreateCollection
 func (c CollectionManager) CreateNewCollection(fabric, collectionName string, allowUserKeys bool, collectionType collection_req.CollectionType) (err error) {
 	req := collection_req.NewRequestForCreateNewCollection(fabric, collectionName, allowUserKeys, collectionType)
 	response := collection_req.NewResponseForCreateNewCollection()
@@ -31,6 +57,9 @@ func (c CollectionManager) CreateNewCollection(fabric, collectionName string, al
 	return nil
 }
 
+// GetCollectionInfo
+// Fetches the information about collection.
+// https://macrometa.com/docs/api#/operations/handleCommandGet:collectionGetProperties
 func (c CollectionManager) GetCollectionInfo(fabric, collectionName string) (response *collection_req.ResponseForGetCollectionInfo, err error) {
 	req := collection_req.NewRequestForGetCollectionInfo(fabric, collectionName)
 	response = collection_req.NewResponseForGetCollectionInfo()
@@ -40,6 +69,8 @@ func (c CollectionManager) GetCollectionInfo(fabric, collectionName string) (res
 	return response, nil
 }
 
+// CheckCollectionExists
+// Returns true if the collection of the name exists. False otherwise.
 func (c CollectionManager) CheckCollectionExists(fabric, collectionName string) (exists bool, err error) {
 	req := collection_req.NewRequestForGetCollectionInfo(fabric, collectionName)
 	response := collection_req.NewResponseForGetCollectionInfo()
@@ -53,6 +84,19 @@ func (c CollectionManager) CheckCollectionExists(fabric, collectionName string) 
 	return true, nil
 }
 
+// CountCollection
+// Returns the number of documents in the collection.
+// Note: This always loads the collection into memory.
+//
+//The call returns a JSON object with at least the following attributes on success:
+//error: false
+//count: The number of documents inside the collection.
+//id: The id of collection as string.
+//name: The name of collection as string.
+//isSystem: True if the collection is a system collection.
+//searchEnabled: True if the collection is searchable.
+//globallyUniqueId: Global unique identifier as string.
+// https://macrometa.com/docs/api#/operations/handleCommandGet:getCollectionCount
 func (c CollectionManager) CountCollection(fabric, collectionName string) (response *collection_req.ResultFromCollection, err error) {
 	req := collection_req.NewRequestForCountCollection(fabric, collectionName)
 	response = collection_req.NewResultFromCollection()
@@ -64,6 +108,29 @@ func (c CollectionManager) CountCollection(fabric, collectionName string) (respo
 
 // UpdateCollectionProperties updates collection properties.
 // Note: except for waitForSync and hasStream, collection properties cannot be changed once a collection is created.
+// Changes the properties of a collection.
+//Requires a JSON object with these properties:
+//
+//hasStream: True if creating a live collection stream.
+//waitForSync: True if waiting for documents to be synchronized to storage.
+//The call returns a JSON object with at least the following attributes on success:
+//id: The identifier of the collection.
+//name: The name of the collection.
+//status: The status of the collection as number.
+//1: new born collection
+//2: unloaded
+//3: loaded
+//4: in the process of being unloaded
+//5: deleted
+//6: loading
+//Every other status indicates a corrupted collection.
+//type: The type of the collection as number.
+//2: document collection (normal case)
+//3: edges collection
+//isSystem: True then the collection is a system collection.
+//stream: True if the collection has a local streams associate with it.
+//Note: except for waitForSync and hasStream, collection properties cannot be changed once a collection is created.
+// https://macrometa.com/docs/api#/operations/handleCommandPut:stream
 func (c CollectionManager) UpdateCollectionProperties(fabric, collectionName string, properties *collection_req.UpdateOptions) (response *collection_req.ResponseForUpdateCollection, err error) {
 	req := collection_req.NewRequestForUpdateCollection(fabric, collectionName, properties)
 	response = collection_req.NewResponseForUpdateCollection()
@@ -73,6 +140,9 @@ func (c CollectionManager) UpdateCollectionProperties(fabric, collectionName str
 	return response, nil
 }
 
+// TruncateCollection
+// Remove all documents from the collection but leaves the indexes intact.
+// https://macrometa.com/docs/api#/operations/handleCommandPut:truncateCollection
 func (c CollectionManager) TruncateCollection(fabric, collectionName string) (response *collection_req.ResponseForTruncateCollection, err error) {
 	req := collection_req.NewRequestForTruncateCollection(fabric, collectionName)
 	response = collection_req.NewResponseForTruncateCollection()
@@ -82,6 +152,11 @@ func (c CollectionManager) TruncateCollection(fabric, collectionName string) (re
 	return response, nil
 }
 
+// DeleteCollection
+// Drops the collection identified by collection-name. If the collection was successfully dropped, an object is returned with the following attributes:
+//error: false
+//id: The identifier of the dropped collection.
+// https://macrometa.com/docs/api#/operations/handleCommandDelete:collection
 func (c CollectionManager) DeleteCollection(fabric, collectionName string, isSystem bool) (err error) {
 	req := collection_req.NewRequestForDeleteCollection(fabric, collectionName, isSystem)
 	response := collection_req.NewResponseForDeleteCollection()
